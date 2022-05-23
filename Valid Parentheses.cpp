f7529e59c388dad041e127d994a025add4294a31
stiff-master22 <105989963+stiff-master22@users.noreply.github.com>
2022-05-24 01:21:31 +0530

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s)
+            {   
+                
+                stack<char> para;
+                for(int i=0; i<s.length();i++)
+                {
+                    if(s[i]== '(' || s[i]== '{' || s[i]== '[')
+                    {
+                        para.push(s[i]);
+                    }
+                   else 
+                    {
+                        if(para.empty())
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if(para.top()=='(' && s[i]==')' || para.top()=='{' && s[i]=='}' || para.top()=='[' && s[i]==']')
+                            {
+                                para.pop();
+                            }
+                            else
+                            {
+                                para.push(s[i]);
+                            }
+                        }
+                    }  
+                }
+                if(para.empty())
+                   return true;
+                else
+                   return false;
+            }
+};
